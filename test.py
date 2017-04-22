6a2201505ccc1960dd27303ff61d5a517f0fb1da
Arturs Kurzemnieks <arturs.kurzemnieks@gmail.com>
2017-04-22 22:40:46 +0300

    trying 2 layers

--- test.py
+++ test.py
@@ -109,14 +109,14 @@ with tf.device('/gpu:0'):
 
             # result = h3
             # result = conv(h1, 3, 3, 1, name='gen_conv')
-            transpose1 = conv_transpose(init_noise, 3, 9, 2, name='gen_transpose1')
+            transpose1 = conv_transpose(init_noise, 3, 4, 4, name='gen_transpose1')
             tf.summary.image('First layer', transpose1)
 
             transpose2 = conv_transpose(transpose1, 3, 3, 2, name='gen_transpose2')
             tf.summary.image('Second layer', transpose2)
 
-            transpose3 = conv_transpose(transpose2, 3, 3, 2, name='gen_transpose3')
-            result = transpose3
+            # transpose3 = conv_transpose(transpose2, 3, 3, 2, name='gen_transpose3')
+            result = transpose2
             tf.summary.image('Output image', result)
 
         vgg = vgg16.Vgg16()
