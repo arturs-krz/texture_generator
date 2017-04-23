e188e4f958a8d424d1bf5db0402b49360195e909
Arturs Kurzemnieks <arturs.kurzemnieks@gmail.com>
2017-04-23 12:43:08 +0300

    changed filter sizes

--- test.py
+++ test.py
@@ -112,13 +112,13 @@ with tf.device('/gpu:0'):
 
             # result = h3
             # result = conv(h1, 3, 3, 1, name='gen_conv')
-            transpose1 = conv_transpose(init_noise, 3, 7, 4, name='gen_transpose1')
+            transpose1 = conv_transpose(init_noise, 3, 7, 2, name='gen_transpose1')
             tf.summary.image('First layer', transpose1)
 
             transpose2 = conv_transpose(transpose1, 3, 7, 2, name='gen_transpose2')
             tf.summary.image('Second layer', transpose2)
 
-            transpose3 = conv_transpose(transpose2, 3, 3, 1, name='gen_transpose3')
+            transpose3 = conv_transpose(transpose2, 3, 4, 2, name='gen_transpose3')
             # tf.summary.image('Third layer', transpose3)
 
             # conv1 = conv(transpose3, 3, 3, 1, name='gen_conv1')
