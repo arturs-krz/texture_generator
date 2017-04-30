wrong dimensions taken for gram_loss M value

test.py
@@ -60,9 +60,10 @@ with tf.device('/gpu:0'):
         used_layers = [
             ('conv1_1', 1.0),
             ('conv2_1', 1.0),
-            ('conv3_1', 1.0)
-            # ('conv4_1', 1.0),
-            # ('conv5_1', 1.0)
+            ('conv3_1', 1.0),
+            ('conv4_1', 1.0),
+            ('conv5_1', 1.0),
+            ('conv6_1', 1.0)
         ]
         image_path = "data/pebbles.jpg"
 