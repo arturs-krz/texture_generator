25aa4570e3fc77b7ba19a6ef29991a802cb44c98
Arturs Kurzemnieks <arturs.kurzemnieks@gmail.com>
2017-05-11 14:39:39 +0300

    output directory check and create

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import skimage
 import numpy as np
 import tensorflow as tf
-import os.path
+import os.path, os.makedirs
 import sys, getopt
 
 # from VGG import vgg16
@@ -196,6 +196,9 @@ with tf.device('/gpu:0'):
                     print("Loading existing model...")
                     saver.restore(sess, "data/model_{}.ckpt".format(image_name))
             
+            if not os.path.exists("output/{}/".format(image_name)):
+                os.makedirs("output/{}/".format(image_name))
+
             # batch_size = 1
             # batch = (0.6 * np.random.uniform(-20,20,(1,28,28,3)).astype("float32")) + (0.4 * input_ref)
             
